a3ef3dc014c15d02742d349dd4cf4dec08e79d71
Muhammad Alif Akbar <alif.thetricky@gmail.com>
2018-03-22 11:40:23 +0700

    add python init file to allow lib referencing

---
+++ WebChecker/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Main Git Hook - Automated Link Check folder
+"""
+
+__version__ = "0.1"
---
+++ WebChecker/lib/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+3rd party lib folder
+"""
+
+__version__ = "0.1"
